6f33b4975609251f3b1501c5cbb6aee906e5211f
umutyldrm <umut.yldrm.6299@hotmail.com>
2023-11-20 18:19:44 +0000

    A basic API for returning template based on request.

--- index.js
+++ index.js
@@ -8,25 +8,45 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.route('/api/:date?')
+  .get(function(req, res) {
+    let dateVariable = new Date();
+    if (req.params.date) {
+      dateVariable = new Date(req.params.date);
+    }
 
+    if (dateVariable == "Invalid Date") {
+      dateVariable = new Date(req.params.date * 1);
+      if (dateVariable == "Invalid Date") {
+        res.json({ error: "Invalid Date" })
+      }
+      else {
+        res.json({ unix: dateVariable.getTime(), utc: dateVariable.toUTCString() });
+      }
+    }
+    else {
+      res.json({ unix: dateVariable.getTime(), utc: dateVariable.toUTCString() });
+    }
+  })
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
